c3d4edd44f7d907851eb904b33252744596ae1d3
zcky <zackyfirdaus934@gmail.com>
2025-07-15 18:18:33 +0700

    first

---
+++ hello.py
@@ -0,0 +1,8 @@
+while True:
+ print("\n")
+ print("========== Selamat Datang ==========")
+ print("====== Di program Pengecekan ======")
+ print("============= password =============")
+ print("\n\n")
+ pw = input("masukkan passowrd: ")
+ print(f"{pw}")

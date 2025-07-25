71d62ef2068dc987205fb4016768bfac45fe5bb0
zacky <zackyfirdaus934@gmail.com>
2025-07-25 08:06:09 +0700

    selesai program pythonnnya

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-import time
+import time #liblary untuk time.sleep()
 
 i = 1
-while i<4:
-    i += 1
-    simbol_unik = set("~`?/{}[]\"':;>.<,!@#$%^&*()_-+=\\|" )
+while i<2:
+    i += 1#increment loop
+    simbol_unik = set("~`?/{}[]\"':;>.<,!@#$%^&*()_-+=\\|" ) # manual simbol
 
-
-    skor = 0
+#nilai awal skor adalah 5, tiap kondisi if true akan berkurang 1, jika false nilai akan tetap.
+    skor =5 
     print("\n")
     print("========== Selamat Datang ==========")
     print("====== Di program Pengecekan ======")
@@ -15,40 +15,41 @@ while i<4:
     pw = input("masukkan password: ")
     if len(pw) <=5:
         print("panjang password minimal 6 karakter")
-        skor += 1
+        skor -= 1 
         time.sleep(2)
-    if not any(c.isupper() for c in pw):
+    if not any(c.isupper() for c in pw): #ada huruf besar?
         print("harus ada huruf kapital")
-        skor += 1
+        skor -= 1
         time.sleep(2)
-    if not any(C.islower() for C in pw):
+    if not any(C.islower() for C in pw): # ada huruf kecil?
         print("harus ada huruf kecil")
-        skor += 1
+        skor -= 1
         time.sleep(2)
-    if not any(a.isdigit() for a in pw):
+    if not any(a.isdigit() for a in pw): #apakah ada angka?
         print("harus ada angka")
-        skor += 1
+        skor -= 1
         time.sleep(2)
-    if not any(s in simbol_unik for s in pw):
+    if not any(s in simbol_unik for s in pw): #ada simbol unik?
         print ("harus ada simbol unik")
-        skor += 1
+        skor -= 1
         time.sleep(2)
     print("\n\n")
 
-#buggggg skor nya jalan terus
+
+#pengecekan kondisi dan memberikan status informasi apakah 
+#password lemah,medium,kuat, berdasarkan skor dari kondisi sebelumnya 
     if skor <= 2:
-        print("your password too weak!")
+        print(f"skor password anda : {skor}, your password too weak!")
         time.sleep(2)
     if skor == 3:
-        print("your password strength is medium")
+        print(f"skor password anda: {skor}, your password strength is medium")
         time.sleep(2)
     if skor >= 4:
-        print("your password is strong")
+        print(f"skor password anda : {skor}, your password is strong")
         time.sleep(2)
     time.sleep(3)
 
-#this code will be develop in the future
-#program for sleep,  this just for debug if program can exit from while loop
 print("\n\n")
-time.sleep(5)
-print("keluar dari while")
+time.sleep(5) #sleep 5 detik
+print("Terimakasih sudah menggunakan program pengecekan password ini.")
+print("tetap waspada celah keamanan disekitar anda, kejahatan tidak tidur.")
